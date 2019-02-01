79fb20f1d04f5bac55c35e7964f448d8e6bbc084
Aric Hylton <arich212@gmail.com>
2019-02-01 05:54:39 -0600

    MongoDB-delete commit

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+       return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').deleteMany({name: 'Aric Hylton'}).then((res) => {
+    //     console.log(res);
+    // });
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID ('5c50f562e4815a4338f8eeae')
+    }).then((res) => {
+        console.log(JSON.stringify(res, undefined, 2));
+    });
+
+    // db.close();
+}); 
