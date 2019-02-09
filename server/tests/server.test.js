28f0ee0a8143903c338e560095de95f754483416
Aric Hylton <arich212@gmail.com>
2019-02-09 15:52:33 -0600

    Add PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,33 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).not.toBeTruthy(); 
+                    done();   
+                }).catch((e) => done(e));               
+            });
+    });
+
+    it('should return 404 if objectID is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+});
