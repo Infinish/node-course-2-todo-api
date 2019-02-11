af90935bd37308648ee2bd8259c4c9467b2ea35a
Aric Hylton <arich212@gmail.com>
2019-02-11 05:23:09 -0600

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 let {mongoose} = require('./db/mongoose.js');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+let {authenticate} = require('./middleware/authenticate');
 
 let app = express();
 const port = process.env.PORT;
@@ -111,6 +112,11 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
